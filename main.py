1430ebc3d59032ddd8b1fbab5f08857dc3d8596f
Md. Arafat Rahman <128305678+CodeMechanicx307@users.noreply.github.com>
2024-12-22 09:21:53 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+import pygame
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+import google.generativeai as genai
+from gtts import gTTS
+import os
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi = "1b1fcbcbb0f746e784675aa44b3259a8"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+def speak(text):
+    tts= gTTS(text)
+    tts.save('text.mp3')
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('text.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    pygame.mixer.music.unload()
+    os.remove("text.mp3")
+
+
+def aiprocess(command):
+
+    genai.configure(api_key="AIzaSyBNTdvKRd7V7mtGAS6YssWu1b1T-XN1ad4")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    chat = model.start_chat(
+        history=[
+            {"role": "user", "parts": command},
+            {"role": "model", "parts": "You are a virtual Assistant named Jarvis , skilled in general tasks like Alexa and GPT-4 give short responses please"},
+        ]
+)
+    response =  chat.send_message(chat.history[1])
+    return response.text
+
+def processcommand(c):
+    if 'open google' in c.lower():
+        webbrowser.open("https://google.com")
+    elif 'open facebook' in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif 'open youtube' in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif 'open github' in c.lower():
+        webbrowser.open("https://github.com")
+    elif 'open eshikhon' in c.lower():
+        webbrowser.open("https://eshikhon.com/")
+    elif 'open gmail' in c.lower():
+        webbrowser.open("https://mail.google.com/")
+    elif 'open fast' in c.lower():
+        webbrowser.open("https://fast.com/")
+    elif 'open wiki' in c.lower():
+        webbrowser.open("https://wikipedia.org/")
+    elif 'show music' in c.lower():
+        print(musicLibrary.music)
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    elif "news" in c.lower():
+        r = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=1b1fcbcbb0f746e784675aa44b3259a8")
+        if r.status_code == 200:
+            data = r.json()
+            articles = data.get('articles',[])
+            for article in articles:
+                speak(article['title'])
+    else:
+        output = aiprocess(c)
+        speak(output)
+    
+    
+   
+
+    
+
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis...")
+    # obtain audio from the microphone
+    while True:
+        r = sr.Recognizer()
+       
+        print("Recognizing...")
+        # recognize speech using Sphinx
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source,timeout=2,phrase_time_limit=1)
+            word =  r.recognize_google(audio)
+            if (word.lower() == 'jarvis'):
+                speak('Yeh')
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command =  r.recognize_google(audio)
+                    processcommand(command)
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "ag":"https://youtu.be/GvXDq-P1NB8?si=RnWoBenQ5xBdOwJa",
+    "sajna":"https://youtu.be/JRKk7gxcpMc?si=P6rWHR4Emizy_DYo",
+    "jana":"https://youtu.be/0PyHEaoZE1c?si=wRKg0o5TMo5XtfBd",
+    "heeriye":"https://youtu.be/RLzC55ai0eo?si=yOGEq65T0BP4Y687",
+    "Ekdin":"https://youtu.be/HXCdQQWLm5A?si=TPovgCq_mbdcKf5e",
+    "baton":"https://youtu.be/XLqmL9cPN1E?si=3QBNIv9q0AiIHT-h",
+    "nashe":"https://youtu.be/CYmGrNSPCiQ?si=IX4sWzv0rfW7HWjE"
+}
